15bc4be3dad8dd02772a0ca1f56480db24954c39
Abhishek Choudhary <abhishekchoudhary236@gmail.com>
2024-02-22 15:57:39 +0530

    created gsap project

---
+++ script.js
@@ -0,0 +1,11 @@
+gsap.to("#box",{
+    x:500,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"green",
+    scaleX:2,
+    scaleY:4,
+    repeat:1, //-1 for infinite
+    yoyo:true
+})
